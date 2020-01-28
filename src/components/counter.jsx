84cebebf14e554c100904503e3533d1f13f5c90e
adyprajwal <prajwal.ady@gmail.com>
2020-01-28 12:30:00 +0545

    add dynamic count button

---
+++ src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+    state = {
+        count: 0
+    };
+    style = {
+        fontSize: 10,
+        fontWeight: "bold"
+    };
+    render() {
+        return (
+            <div>
+                <span style={this.style} className={this.getBadgeClasses()}>
+                    {this.formatCount()}
+                </span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+}
+
+export default Counter;
