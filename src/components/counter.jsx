6dfeaa9927ba54e7b85d7d778ad53d861d3998ca
adyprajwal <prajwal.ady@gmail.com>
2020-01-31 13:01:37 +0545

    add handleDelete method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: []
+        value: this.props.value
+        // tags: []
     };
     style = {
         fontSize: 10,
@@ -17,9 +17,10 @@ class Counter extends Component {
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
 
-    handleIncrement = product => {
-        this.setState({ count: this.state.count + 1 });
+    handleIncrement = () => {
+        this.setState({ value: this.state.value + 1 });
     };
+
     render() {
         return (
             <div>
@@ -31,6 +32,12 @@ class Counter extends Component {
                     className="btn btn-secondary btn-sm"
                 >
                     Increment
+                </button>
+                <button
+                    onClick={() => this.props.onDelete(this.props.id)}
+                    className="btn btn-danger btn-sm m-2"
+                >
+                    Delete
                 </button>
                 {/* <ul>
                     {this.state.tags.map(tag => (
@@ -56,12 +63,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
