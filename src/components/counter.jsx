2f745bde92565f6483f3295f6a8028bb7a2823dc
adyprajwal <prajwal.ady@gmail.com>
2020-01-31 14:56:07 +0545

    refactor code to remove local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value
-        // tags: []
-    };
     style = {
         fontSize: 10,
         fontWeight: "bold"
@@ -17,58 +13,38 @@ class Counter extends Component {
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
 
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 });
-    };
-
     render() {
         return (
             <div>
                 <span style={this.style} className={this.getBadgeClasses()}>
                     {this.formatCount()}
                 </span>
+
                 <button
-                    onClick={() => this.handleIncrement()}
+                    onClick={() => this.props.onIncrement(this.props.counter)}
                     className="btn btn-secondary btn-sm"
                 >
                     Increment
                 </button>
+
                 <button
                     onClick={() => this.props.onDelete(this.props.counter.id)}
                     className="btn btn-danger btn-sm m-2"
                 >
                     Delete
                 </button>
-                {/* <ul>
-                    {this.state.tags.map(tag => (
-                        <li key={tag}>{tag}</li>
-                    ))}
-                </ul> */}
-                {/* {this.state.tags.length === 0 && "Please create new tags"}
-                {this.renderTags()} */}
             </div>
         );
     }
 
-    // renderTags() {
-    //     if (this.state.tags.length === 0) return "There are no tags!";
-    //     return (
-    //         <ul>
-    //             {this.state.tags.map(tag => (
-    //                 <li key={tag}>{tag}</li>
-    //             ))}
-    //         </ul>
-    //     );
-    // }
-
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { value: count } = this.state;
+        const { value: count } = this.props.counter;
         return count === 0 ? "Zero" : count;
     }
 }
