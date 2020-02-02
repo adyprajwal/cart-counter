0cffc42752ac4f9aed70f637c03f830628a99f3c
adyprajwal <prajwal.ady@gmail.com>
2020-02-02 13:25:46 +0545

    refactor -> convert navbar component into stateless functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Items
-                    <span className="m-2 badge badge-pill badge-secondary">
-                        {this.props.totalCounters}
-                    </span>
-                </a>
-            </nav>
-        );
-    }
-}
+//Stateless Functional Components
+const Navbar = props => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Items
+                <span className="m-2 badge badge-pill badge-secondary">
+                    {props.totalCounters}
+                </span>
+            </a>
+        </nav>
+    );
+};
+
+//Above Functional Component works same as the class based component below
+//While we have to define the simple components which doesn't have states then
+//it is better to use stateless functional components
+
+// class NavBar extends Component {
+//     render() {
+//         return (
+//             <nav className="navbar navbar-light bg-light">
+//                 <a className="navbar-brand" href="#">
+//                     Items
+//                     <span className="m-2 badge badge-pill badge-secondary">
+//                         {this.props.totalCounters}
+//                     </span>
+//                 </a>
+//             </nav>
+//         );
+//     }
+// }
 
 export default NavBar;
