27cff3e9981113af25410250048ec1f103a4153a
adyprajwal <prajwal.ady@gmail.com>
2020-02-02 12:57:10 +0545

    refactor -> lift state to 'App' component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,45 @@ import Counters from './components/counters'
 
 
 class App extends Component {
-	state = {  }
+	state = {
+        counters: [
+            { id: 1, value: 0 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 }
+        ]
+    };
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
+    };
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
+    };
+
 	render() { 
 		return ( 
 			<React.Fragment>
 				<NavBar/>
 				<main clasName="container">
-					<Counters/>
+					<Counters
+					counters={this.state.counters}
+					onReset={this.handleReset}
+					onIncrement={this.handleIncrement}
+					onDelete={this.handleDelete}
+					/>
 				</main>
 			</React.Fragment>
 		 );
